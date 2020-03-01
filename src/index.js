ec0573732c1fc0e7e92c1d430bb7a85d3a6c41a1
Dzmitry <dim0n.by@gmail.com>
2020-03-02 00:07:41 +0300

    fix: edit index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array && isNaN(array) && (array.length  != 0)) {
+    array.sort((a, b) => a - b);
+    return (array[0]);
+  }
+  else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array && isNaN(array) && (array.length  != 0)) {
+  array.sort((a, b) => b - a);
+ return (array[0]);
+  }
+  else {
+    return 0;
+  }
+ 
 }
 
 exports.avg = function avg (array) {
+  if (array && isNaN(array) && (array.length  != 0)) {
+  function arraySum(arr){
+  var sum = 0;
+  for(var i = 0; i < arr.length; i++){
+      sum += arr[i];
+      }
+  return(sum);
+  }
+  var totalAverage=(arraySum(array) / array.length);
+  return totalAverage;
+}
+else {
   return 0;
+}
 }
